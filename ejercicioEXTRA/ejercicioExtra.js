ca6b36c63dbd844fc5a5a8ba791b6bd6e1fa9994
sonia526 <zamirastars3@gmail.com>
2017-07-18 14:50:24 -0500

    archivos html y js

---
+++ ejercicioEXTRA/ejercicioExtra.js
@@ -0,0 +1,11 @@
+
+function calcular(celcius){
+   var farengeit=celcius*(9/5)+5;
+  return farengeit;
+}
+
+ function ConvertirCelsius(){
+
+
+farengeit.innerText=calcular( document.getElementById('celcius').value);
+}
